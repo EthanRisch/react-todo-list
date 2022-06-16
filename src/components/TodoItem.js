9fed61eb52937f7de91ee10f424208d7ffda3c64
Ethan Risch <60640087+EthanRisch@users.noreply.github.com>
2022-06-16 14:29:33 -0500

    Integration w/ firebase for saving todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 import classes from "./TodoItem.module.css";
 
 import Card from "../ui/Card";
@@ -7,11 +9,23 @@ import check from "./img/Vectorcheck.svg";
 import hamburger from "./img/Vectoroptions.svg";
 
 function TodoItem(props) {
+  const titleInputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = titleInputRef.current.value;
+
+    props.onEditTitle(title, props.id);
+  };
+
   return (
     <Card>
       <section>
         <span className={classes.text}>
-          <h3>{props.title}</h3>
+          {/*<h3>{props.title}</h3>*/}
+          <form action="" onSubmit={handleSubmit}>
+            <input type="text" placeholder={props.title} ref={titleInputRef} />
+          </form>
           <button>
             <img src={hamburger} alt="" />
           </button>
